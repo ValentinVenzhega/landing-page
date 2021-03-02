5c8c89897f583cee97e8346ac252cd77c43c1871
Valentin Venzhega <valek917@yandex.ru>
2021-03-02 23:28:33 +0300

    задание 24 выполнено

--- script/script.js
+++ script/script.js
@@ -415,38 +415,54 @@ window.addEventListener('DOMContentLoaded', function() {
       calcDay = document.querySelector('.calc-day'),
       calcCount = document.querySelector('.calc-count'),
       totalValue = document.getElementById('total');
-   
+
+      
+
+      // num - число, elem -куда будем записывать
+      const outNum = (num, elem) => {
+         const time = 1500, // время
+            step = 100; // шаг
+            
+         let n = 0;
+         let t = Math.round(time/(num/step));
+         let interval = setInterval(() => {
+            n = n + step;
+            if (n >= num) {
+               clearInterval(interval);
+            }
+            elem.innerHTML = n;
+         }, t);
+      };
+      
       const countSum = () => {
          let total = 0,
          countValue = 1,
          dayValue = 1;
-         const typeValue = calcType.options,
+         const typeValue = calcType.options[calcType.selectedIndex].value,
             squareValue = +calcSquare.value;
-            console.log(typeValue);
    
+         if (calcCount.value > 1) {
+            countValue += (calcCount.value - 1) / 10;
+         }
 
-   
-            if (calcCount.value > 1) {
-               countValue += (calcCount.value - 1) / 10;
-            }
-   
-            if (calcDay.value && calcDay.value < 5) {
-               dayValue *= 2;
-            } else if (calcDay.value && calcDay.value < 10) {
-               dayValue *= 1.5;
-            }
-   
-            if (typeValue && squareValue) {
-               total = price * typeValue * squareValue * countValue * dayValue;
-            }
-   
-            totalValue.textContent = 10;
+         if (calcDay.value && calcDay.value < 5) {
+            dayValue *= 2;
+         } else if (calcDay.value && calcDay.value < 10) {
+            dayValue *= 1.5;
+         }
+
+         if (typeValue && squareValue) {
+            total = price * typeValue * squareValue * countValue * dayValue;
+         }
+
+         outNum(total, totalValue);
       };
    
       calcBlock.addEventListener('change', (event) => {
          const target = event.target;
          if (target.matches('select') || target.matches('input')) {
             countSum();
+            
          }
       });
    };
