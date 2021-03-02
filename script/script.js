f8b4c6470cc10bf0b0cfd6bc9924ea0f32f93d13
Valentin Venzhega <valek917@yandex.ru>
2021-03-02 22:01:32 +0300

    задание 23 выполнено

--- script/script.js
+++ script/script.js
@@ -276,14 +276,14 @@ window.addEventListener('DOMContentLoaded', function() {
       const  commandPhoto = document.querySelectorAll('.command__photo');
 
       commandPhoto.forEach(elem => {
+         const srcElement = elem.src;
          elem.addEventListener('mouseenter', (event) => {
             const target = event.target;
             target.src = target.dataset.img;
          }); 
          elem.addEventListener('mouseleave', (event) => {
             const target = event.target;
-            // удаляю http://127.0.0.1:5500/    // удаляю a перед точкой
-            target.src = (target.src.replace(/^.*?:\/\/.*?(?=\/|$)\//,'')).replace(/a\./, '.');
+            target.src = srcElement;
          }); 
       });
    };
